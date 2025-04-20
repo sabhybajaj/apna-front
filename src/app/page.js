d307f6f817962a5ff4a9f7a9caafd6242981d4e6
Sabhy Bajaj <sabhybajaj61@gmail.com>
2025-04-20 21:25:55 +0530

    cdcdc

---
+++ src/app/page.js
@@ -0,0 +1,20 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
+
+
+export default function Home() {
+
+  const router = useRouter();
+
+  useEffect(()=>{
+    router.push("/login"); 
+  },[])
+
+
+  return (
+    "heelo world!!"
+  );
+}
+
